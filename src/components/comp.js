ccaae0453b26701024bc7705f72acc924c5fd656
Shivani Anandan <shivanianandhan28@gmail.com>
2023-06-12 20:50:40 +0530

    Final commit

--- src/components/comp.js
+++ src/components/comp.js
@@ -3,7 +3,7 @@ import React from "react";
 function Comp(){
 
     return(
-        <div id='root'>
+        <div>
             <p>I am learning React. My life is getting better.</p>
         </div>
     )
